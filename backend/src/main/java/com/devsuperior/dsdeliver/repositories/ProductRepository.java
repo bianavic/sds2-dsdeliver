d818239100b26e3c5b42017dc0905178ac879cd7
bianavic <bianavic@gmail.com>
2021-01-06 17:33:18 -0300

    GET /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/repositories/ProductRepository.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/repositories/ProductRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.devsuperior.dsdeliver.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long>{
-	List<Product> findAllByOrderByNameAsc();
 	
+	List<Product> findAllByOrderByNameAsc();
 }
