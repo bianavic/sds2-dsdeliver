a3379ee5b1c88af541958101013e9c35aa451508
bianavic <bianavic@gmail.com>
2021-01-07 12:46:26 -0300

    add home page

---
+++ front-web/src/Navbar/index.tsx
@@ -0,0 +1,14 @@
+import './styles.css';
+import { ReactComponent as Logo } from './logo.svg';
+
+function Navbar() {
+    return (
+        <nav className="main-navbar">
+            <Logo />
+            <a href="home" className='logo-text'>DS Delivey</a>
+
+        </nav>
+    )
+}
+
+export default Navbar;
