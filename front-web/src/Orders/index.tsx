77ede913ed64424aab4c3d00898c2e78abdbe0cc
bianavic <bianavic@gmail.com>
2021-01-08 09:20:35 -0300

    add routes and StepsHeader

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,24 @@
-import StepsHeader from './StepsHeader';
 import './styles.css';
+import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
+import { Product } from './types';
+import { fetchProducts } from '../api';
+import { useEffect, useState } from 'react';
 
 function Orders() {
+    const [products, setProducts] = useState<Product[]>([]);
+    console.log(products);
+
+    useEffect(() => {
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(error => console.log(error))
+        }, []);
+
     return (
         <div className="orders-container">
             <StepsHeader />
+            <ProductsList products={products} />
         </div>
     )
 }
